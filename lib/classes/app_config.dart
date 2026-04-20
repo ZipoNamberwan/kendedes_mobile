17ef1103d3b2f1c3993b05231b71b6a7df9e026d
Zipo Namberwan <dopokan@gmail.com>
2026-04-20 21:33:00 +0700

    release v1.2 -- browse mode

--- lib/classes/app_config.dart
+++ lib/classes/app_config.dart
@@ -1,7 +1,7 @@
 class AppConfig {
   // static const String baseurl = 'http://192.168.1.7:8888';
-  static const String baseurl = 'http://10.35.0.98:8888';
-  // static const String baseurl = 'https://kendedes.cathajatim.id';
+  // static const String baseurl = 'http://10.35.0.98:8888';
+  static const String baseurl = 'https://kendedes.cathajatim.id';
   static const String apiUrl = '$baseurl/api';
   static const String updateUrl = 'https://s.bps.go.id/kendedes';
   static const String majapahitLoginUrl =
